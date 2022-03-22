b0c5fa8bf974b8fefb236ad5a1991adea079aebd
Spulga <kristinei.marcinkevicai@gmail.com>
2022-03-22 22:02:06 +0200

    add exibition images

--- src/Pages/Exibitions.js
+++ src/Pages/Exibitions.js
@@ -7,18 +7,22 @@ function Exibitions() {
 
   const exibitionsList = exibitons.map((exibiton, index) => {
     return (
-      <div className="col-md-4 font-family p-0" key={index}>
-        <div className="card text-white border-0 p-3" >
+      <div className="col-md-4 font-family p-0 " key={index}>
+        <div className="card text-white border-0 p-1 ">
           <Link to={`/exibitions/${index}`}>
-            <img className="card-img-top" src={exibiton.image} alt="article" style={{width: "40vh", height: "40vh", objectFit: "cover"}} />
+            <img
+              className="card-img-top exibition-poster-images"
+              src={exibiton.image}
+              alt="article"
+              // style={{ width: "35vh", height: "35vh", objectFit: "cover" }}
+            />
           </Link>
-          <div className="card-body ">
-            <h5 className="card-title text-center">
-              <Link className="linkDecoration" to={`/exibitions/${index}`}>
-                {exibiton.title}
-              </Link>
-            </h5>
-          </div>
+
+          <Link className="linkDecoration" to={`/exibitions/${index}`}>
+            <div className="card-body ">
+              <h6 className="card-title text-center" style={{height: "1vh"}}>{exibiton.title}</h6>
+            </div>
+          </Link>
         </div>
       </div>
     )
